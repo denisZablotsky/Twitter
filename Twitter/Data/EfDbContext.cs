62c4887f96142aa47e19599edda279894a6bbd4f
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-15 18:54:21 +0700

    Data Base Context

---
+++ Twitter/Data/EfDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Twitter.Models;
+using System.Data.Entity;
+
+namespace Twitter.Data
+{
+    public class EfDbContext: DbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<UserProfile> UserProfiles { get; set; }
+        DbSet<Tweet> Tweets { get; set; }
+    }
+}

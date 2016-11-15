2d40676f2a6043b5030c2503c8307ce232d24898
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-15 20:57:35 +0700

    User Page

---
+++ Twitter/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twitter.Data;
+using Twitter.Models;
+
+namespace Twitter.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository userRepository;
+        private ITweetRepository tweetRepository;
+        public UserController(IUserRepository userRep, ITweetRepository tweetRep)
+        {
+            userRepository = userRep;
+            tweetRepository = tweetRep;
+        }
+        // GET: User
+        public ActionResult Index(int id)
+        {                
+            User user = userRepository.GetUserById(id);
+            if (user == null)
+                return RedirectToAction("Index", "Home");
+            return View(user);
+        }
+        public PartialViewResult CreateTweet(int id)
+        {
+            Tweet tweet = new Tweet();
+            tweet.UserId = id;
+            return PartialView(tweet);
+        }
+        [HttpPost]
+        public ActionResult CreateTweet(Tweet tweet)
+        {
+            tweet.UserId = tweet.Id;
+            tweetRepository.CreateTweet(tweet);
+            return RedirectToAction("Index", "User", new { id = tweet.UserId });
+        }
+    }
+}

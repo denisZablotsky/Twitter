af3453cc1c9535393ad295890a1e43790d45fa28
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-15 18:33:50 +0700

    Tweet model and dependence with User

---
+++ Twitter/Models/Tweet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Twitter.Models
+{
+    public class Tweet
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatingDate { get; set; }
+
+        /*
+         * Navigation Property. 
+         * |User| -HasMany-> |Tweet|
+         */
+        public virtual int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
--- Twitter/Models/User.cs
+++ Twitter/Models/User.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Collections.ObjectModel;
 
 namespace Twitter.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime SignupData { get; set; }
+
+        public virtual ICollection<Tweet> Tweets { get; set; }
+
+        public User()
+        {
+            Tweets = new Collection<Tweet>();
+        }
     }
 }

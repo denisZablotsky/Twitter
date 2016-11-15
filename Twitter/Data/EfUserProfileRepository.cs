5a7dd15b1605ec3700ab25afc160019ea29e565d
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-15 19:06:36 +0700

    Repository for User Profile

---
+++ Twitter/Data/EfUserProfileRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Twitter.Models;
+
+namespace Twitter.Data
+{
+    public class EfUserProfileRepository : IUserProfileRepository
+    {
+        EfDbContext context;
+        public EfUserProfileRepository()
+        {
+            context = new EfDbContext();
+        }
+        public UserProfile CreateUserProfile(UserProfile userProfile)
+        {
+            UserProfile newUserProfile = context.UserProfiles.Add(userProfile);
+            context.SaveChanges();
+            return newUserProfile;
+        }
+    }
+}

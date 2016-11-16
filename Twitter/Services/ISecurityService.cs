b2a97d45059d402c980c60d0d3465fa3ac6efaa1
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-16 18:41:24 +0700

    Design of Security Service for authenticating

---
+++ Twitter/Services/ISecurityService.cs
@@ -0,0 +1,13 @@
+﻿using Twitter.Models;
+
+namespace Twitter.Services
+{
+    public interface ISecurityService
+    {
+        bool IsAuthenticate();
+        bool Authenticate(string name, string password);
+        void Login(User user);
+        void Logout();
+        User GetCurrentUser();
+    }
+}

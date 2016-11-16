eac88408edda2220805c5250b12ae12514ada753
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-16 19:22:38 +0700

    Basic Page of authenticated user

---
+++ Twitter/Controllers/MeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twitter.Models;
+using Twitter.Services;
+
+namespace Twitter.Controllers
+{
+    public class MeController : Controller
+    {
+        ISecurityService _security;
+        public MeController(ISecurityService security)
+        {
+            _security = security;
+        }
+        // GET: Me
+        public ActionResult Index()
+        {
+            if (!_security.IsAuthenticate())
+                return RedirectToAction("Index", "Home");
+            User user = _security.GetCurrentUser();
+            return View(user);
+        }
+    }
+}

50b833cef9b3042c361a03ee7fdbedb2e831eb99
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-24 19:34:11 +0700

    Hashtag repository

---
+++ Twitter/Data/EfHashtagRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Twitter.Models;
+
+namespace Twitter.Data
+{
+    public class EfHashtagRepository : IHashtagRepository
+    {
+        private EfDbContext context;
+        public EfHashtagRepository()
+        {
+            context = new EfDbContext();
+        }
+        public IQueryable<Hashtag> Hashtags
+        {
+            get
+            {
+                return context.Hashtags;
+            }
+        }
+
+        public Hashtag CreateHashtag(Hashtag hashtag)
+        {
+            Hashtag Hashtag = context.Hashtags.Add(hashtag);
+            context.SaveChanges();
+            return Hashtag;
+        }
+
+        public Hashtag GetHashtagById(int id)
+        {
+            return context.Hashtags.Find(id);
+        }
+
+        public Hashtag GetHashtagByTag(string tag)
+        {
+            return context.Hashtags.SingleOrDefault(x => x.Tag == tag);
+        }
+    }
+}
---
+++ Twitter/Data/IHashtagRepository.cs
@@ -0,0 +1,13 @@
+﻿using Twitter.Models;
+using System.Linq;
+
+namespace Twitter.Data
+{
+    interface IHashtagRepository
+    {
+        IQueryable<Hashtag> Hashtags { get; }
+        Hashtag GetHashtagByTag(string tag);
+        Hashtag CreateHashtag(Hashtag hashtag);
+        Hashtag GetHashtagById(int id);
+    }
+}

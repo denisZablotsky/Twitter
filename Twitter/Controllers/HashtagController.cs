3fafa594b4b59c7084a2b867bc27ccea6949a1de
Denis Zablotsky <dZablotsky@yandex.ru>
2016-11-24 21:32:12 +0700

    Fixed of invalid link on HashtagView

---
+++ Twitter/Controllers/HashtagController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twitter.Models;
+using Twitter.Data;
+
+namespace Twitter.Controllers
+{
+    public class HashtagController : Controller
+    {
+        private IHashtagRepository hashRepository;
+        public HashtagController(IHashtagRepository hashRep)
+        {
+            hashRepository = hashRep;
+        }
+        // GET: Hashtag
+        public ActionResult Index(string id)
+        {
+            if(id != null)
+            {
+                return View(hashRepository.GetHashtagByTag(id));
+            }
+            return View("Index", null);
+        }
+    }
+}
